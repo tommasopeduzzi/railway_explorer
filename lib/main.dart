c74bac3a88f313374b11b20a192df7f175a1d4bb
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2022-05-18 14:53:24 +0200

    Added location marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
 
 void main() {
@@ -30,8 +32,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void checkPermissions() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    checkPermissions();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -39,7 +49,7 @@ class _HomePageState extends State<HomePage> {
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
-              print("pressed settigns button");
+              print("pressed settings button");
             },
           ),
         ],
@@ -49,14 +59,17 @@ class _HomePageState extends State<HomePage> {
           center: LatLng(47.547484, 7.589800),
           zoom: 13.0,
         ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
-            attributionBuilder: (_) {
-              return Text("© OpenStreetMap contributors");
-            },
+        children: [
+          TileLayerWidget(
+            options: TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+              attributionBuilder: (_) {
+                return Text("© OpenStreetMap contributors");
+              },
+            ),
           ),
+          LocationMarkerLayerWidget(),
         ],
       ),
     );
