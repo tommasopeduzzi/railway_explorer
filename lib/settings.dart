afc8289626fe53071473535e5d5bea1a4191ac08
David Bartsch <david.bartsch@bluewin.ch>
2022-05-31 12:43:03 +0200

    Added offline mode toggle, Colour picker in Settin

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -7,25 +9,80 @@ class Settings extends StatefulWidget {
 
 // Create state
 class _SettingsState extends State<Settings> {
+  bool offlineMode = false;
+  Color railColour = Colors.green;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter a value',
-                ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text('Offline mode', style: TextStyle(fontSize: 25)),
+              Switch(
+                value: offlineMode,
+                activeTrackColor: Colors.lightGreenAccent,
+                activeColor: Colors.green,
+                onChanged: (value) {
+                  setState(() {
+                    offlineMode = value;
+                  });
+                },
               ),
-            ],
-          ),
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Railway colour', style: TextStyle(fontSize: 25)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: TextButton(
+                          child: Text('Pick a Colour'),
+                          onPressed: (() {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Pick a colour'),
+                                    content: SingleChildScrollView(
+                                      child: ColorPicker(
+                                        pickerColor: railColour,
+                                        onColorChanged: (color) {
+                                          setState(() {
+                                            railColour = color;
+                                          });
+                                        },
+                                        pickerAreaHeightPercent: 0.8,
+                                      ),
+                                    ),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text('Close'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                });
+                          }),
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: railColour,
+                        radius: 20,
+                      ),
+                    ])
+              ],
+            )
+          ],
         ),
       ),
     );
